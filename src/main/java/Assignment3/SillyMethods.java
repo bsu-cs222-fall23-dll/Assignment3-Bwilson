1ea7a45b8ce636b1d83937c835098123245e0283
Ben-jaminWilson <122378697+Ben-jaminWilson@users.noreply.github.com>
2023-09-04 22:44:12 -0400

    Initial commit

---
+++ src/main/java/Assignment3/SillyMethods.java
@@ -0,0 +1,14 @@
+package Assignment3;
+public class SillyMethods {
+    public boolean didGuess42(int guess){
+        return guess == 42;
+    }
+    public int countTo(int index) {
+        int[] listedNumbers = new int[index];
+        for (int i = 0; i < index; i++) {
+            listedNumbers[i] = i + 1;
+        return listedNumbers[];
+
+        }
+    }
+}
