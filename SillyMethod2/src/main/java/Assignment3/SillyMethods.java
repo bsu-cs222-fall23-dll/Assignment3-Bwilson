93eac51f496ff5ada0426d5959f9e88131ce61f3
Ben-jaminWilson <122378697+Ben-jaminWilson@users.noreply.github.com>
2023-09-04 23:09:52 -0400

    Add files via upload

---
+++ SillyMethod2/src/main/java/Assignment3/SillyMethods.java
@@ -0,0 +1,16 @@
+package Assignment3;
+public class SillyMethods {
+    public boolean didGuess42(int guess){
+        return guess == 42;
+    }
+    public String countTo(int index) {
+        StringBuilder Array = new StringBuilder();
+        for (int i = 0; i < index; i++) {
+            Array.append(i);
+            if (i != index - 1){
+                Array.append(",");
+            }
+        }
+        return Array.toString();
+    }
+}
